6ab3bb49ef5405b6d1487bc6f5ff2921429627b4
Aurélien Pelletier <aurelien.pelletier@gmail.com>
2011-05-01 23:36:17 +0200

    Flag per user web interface

---
+++ demo/src/main/java/org/featureflags/demo/FeatureFlagFilter.java
@@ -0,0 +1,36 @@
+package org.featureflags.demo;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.featureflags.FlagManager;
+
+public class FeatureFlagFilter implements Filter {
+
+    private FlagManager flagManager;
+    
+    public void init(FilterConfig filterConfig) throws ServletException {
+	String featureFlagsClassName = filterConfig.getInitParameter("featureFlagsClassName");
+	flagManager = FlagManager.get(featureFlagsClassName);
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+	String username = request.getParameter("username");
+	flagManager.setThreadUserName(username);
+	chain.doFilter(request, response);
+	flagManager.resetThreadUserName();
+
+    }
+
+    public void destroy() {
+	// TODO Auto-generated method stub
+
+    }
+
+}
