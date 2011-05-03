d481ab546110e34a37daa300da0eb5776726db14
Aurélien Pelletier <aurelien.pelletier@gmail.com>
2011-05-04 00:10:57 +0200

    Demo update

--- demo/src/main/java/org/featureflags/demo/FeatureFlagFilter.java
+++ demo/src/main/java/org/featureflags/demo/FeatureFlagFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 import org.featureflags.FlagManager;
 
